bdd8809bdd7bd4467ba0560a6a691c9aaa674f4e
vladimir.mindov <vladimir.mindov@mentormate.com>
2018-03-16 16:47:39 +0200

    upd

--- IdentityServerApp/SensitiveData.cs
+++ IdentityServerApp/SensitiveData.cs
@@ -7,6 +7,7 @@ namespace IdentityServerApp
 {
     public class SensitiveData
     {
+        // Configure Google https://identityserver4.readthedocs.io/en/release/quickstarts/4_external_authentication.html
         public string GoogleClientId { get; set; }
         public string GoogleClientSecret { get; set; }
     }
